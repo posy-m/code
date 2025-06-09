894dfed826cb540e120b48455a46b424aabdcf5a
posy-m <minji.posy@gmail.com>
2025-06-09 18:30:52 +0900

    painting

---
+++ painting/app.js
@@ -0,0 +1,73 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+canvas.width = 800;
+canvas.height = 800;
+ctx.lineWidth = 2;
+let isPainting = false
+
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
+}
+function startPainting() {
+  isPainting = true;
+}
+function cancelPainting() {
+  isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+
+
+// const colors = [
+//   "#ff3838",
+//   '#ffb8b8',
+//   '#c56cf0',
+//   '#ff9f1a',
+//   '#fff200',
+//   '#32ff7e',
+//   '#7efff5',
+//   '#18dcff',
+//   '#7d5fff'
+// ]
+
+// function onClick(event) {
+//   ctx.beginPath();
+//   ctx.moveTo(0, 0);
+//   const color = colors[Math.floor(Math.random() * colors.length)];
+//   ctx.strokeStyle = color
+//   ctx.lineTo(event.offsetX, event.offsetY);
+//   ctx.stroke();
+// }
+
+// canvas.addEventListener('mousemove', onClick)
+
+// x, y = 이동 / width, height
+// ctx.rect(50, 50, 100, 100);
+
+// ctx.moveTo(50, 50);
+// ctx.lineTo(150, 50);
+// ctx.lineTo(150, 150);
+// ctx.lineTo(50, 150);
+// ctx.lineTo(50, 50);
+// ctx.fill();
+
+// ctx.fillRect(200, 200, 50, 200);
+// ctx.fillRect(400, 200, 50, 200);
+// ctx.lineWidth = 2;
+// ctx.fillRect(300, 300, 50, 100);
+// ctx.fillRect(200, 200, 200, 20);
+
+// ctx.moveTo(200, 200);
+// ctx.lineTo(325, 100);
+// ctx.lineTo(450, 200);
+// ctx.fill();
+// ctx.stroke();
+
