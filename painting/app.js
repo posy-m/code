09ec664df76052b7ac13f775bef2ea3b5003ac4f
posy-m <minji.posy@gmail.com>
2025-06-18 17:18:02 +0900

    destroybtn, eraserbtn

--- painting/app.js
+++ painting/app.js
@@ -1,14 +1,19 @@
 const modeBtn = document.getElementById('mode-btn')
-
+const destroyByn = document.getElementById('destroy-btn')
+const eraserBtn = document.getElementById('eraser-btn')
 const colorOption = Array.from(
   document.getElementsByClassName('color-option')
 );
 const color = document.getElementById('color')
 const lineWidth = document.getElementById('line-width')
 const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
-canvas.width = 800;
-canvas.height = 800;
+const ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 let isFilling = false;
@@ -61,8 +66,19 @@ function onModeClick() {
 
 function onCanvasClick() {
   if (isFilling) {
-    ctx.fillRect(0, 0, 800, 800)
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
   }
+}
+
+function onDestroyClick() {
+  ctx.fillStyle = 'white'
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+}
+
+function onEraserClick() {
+  ctx.strokeStyle = 'white'
+  isFilling = false;
+  modeBtn.innerText = 'Fill'
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -77,6 +93,9 @@ colorOption.forEach(color => color.addEventListener('click', onColorClick))
 
 
 modeBtn.addEventListener('click', onModeClick)
+destroyByn.addEventListener('click', onDestroyClick)
+eraserBtn.addEventListener('click', onEraserClick)
+
 
 
 
